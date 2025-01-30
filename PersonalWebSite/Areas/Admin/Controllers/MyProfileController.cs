4da3e41fe85a0dd78d5871ea945dd77f9ed4ed29
Emin <erolemin@outlook.com.tr>
2025-01-30 04:20:19 +0300

    created MyProfile and Settings Pages

---
+++ PersonalWebSite/Areas/Admin/Controllers/MyProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/MyProfile")]
+    [Authorize]
+    public class MyProfileController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalWebSite/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Setting")]
+    [Authorize]
+    public class SettingController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
